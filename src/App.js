884d919609972f4070a762b110ea82c7792a35cc
unknown <doananhly1999@gmail.com>
2021-12-18 01:39:19 +0700

    Lap 3: Todo form and add todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { React, useState } from 'react';
 import './App.scss';
+import TodoForm from './components/todoForm';
 import TodoList from './components/todolist/index'
 
 function App() {
@@ -8,18 +9,31 @@ function App() {
     { id: 2, title: 'Testing' },
     { id: 3, title: 'Fixbug' }
   ]);
+
   function handleTodoClick(todo) {
     console.log(todo);
     const index = todoList.findIndex(x => x.id === todo.id);
-    if(index < 0) return;
+    if (index < 0) return;
     const newTodoList = [...todoList];
     newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('For submit', formValues);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
     setTodoList(newTodoList);
   }
   return (
     <div className="app">
       <h1>React hooks - Todolist</h1>
       {/* <ColorBox/> */}
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
